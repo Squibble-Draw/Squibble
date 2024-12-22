ead451d9d8df33ec816131fa75bb33dd2784b870
Ocean0000000 <vantocean509@gmail.com>
2024-12-21 22:15:43 -0800

    Added PWA functionality

---
+++ src/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "drawing-app-cache-v1";
+const urlsToCache = [
+  "/index.html",
+  "/coloringPage.css",
+  "/coloringPage.js",
+  "/style.css",
+  "/public/squirrel-3.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return the response from the cache
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
